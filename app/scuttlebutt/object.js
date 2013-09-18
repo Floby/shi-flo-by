847bc0af4171902b3bb6390fd0b9dab9f7cafa78
Florent Jaby <fjaby@octo.com>
2013-09-19 01:57:24 +0200

    add simple tests for scuttlebutts/ember models

---
+++ app/scuttlebutt/object.js
@@ -0,0 +1,8 @@
+var Model = require('scuttlebutt/model');
+var SbObject = Ember.Object.extend({
+  createReadStream: function () {
+    return { readable: true };
+  }
+})
+
+export default SbObject;
---
+++ tests/unit/scuttlebutt/scuttlebutt_object_test.js
@@ -0,0 +1,22 @@
+import SbObject from 'appkit/scuttlebutt/object';
+
+module('Unit - Scuttlebutt - Object');
+
+
+test('it exists', function () {
+  ok(SbObject);
+  ok(Ember.Object.detect(SbObject));
+});
+
+test('it can be instanciated', function () {
+  var sbo = SbObject.create();
+  ok(sbo instanceof SbObject);
+});
+
+test('it can create a readable Stream for sync', function () {
+  var sbo = SbObject.create();
+  ok(typeof sbo.createReadStream == 'function');
+  var stream = sbo.createReadStream();
+  ok(stream.readable);
+})
+
