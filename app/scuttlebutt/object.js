4d3e9abfe1025fb45a469260e0eddc5c2f7c317f
Florent Jaby <fjaby@octo.com>
2013-09-24 22:55:14 +0200

    read changes from a scuttlebutt

--- app/scuttlebutt/object.js
+++ app/scuttlebutt/object.js
@@ -1,7 +1,21 @@
 var Model = require('scuttlebutt/model');
 var SbObject = Ember.Object.extend({
+  init: function () {
+    var model = this._scuttlebuttModel = new Model();
+    var self = this;
+    model.on('update', function (update) {
+      Ember.run(function () {
+        self.set(update[0], update[1]);
+      })
+    });
+  },
   createReadStream: function () {
-    return { readable: true };
+    Ember.assert('No scuttlebutt model was initialised on this instance', this._scuttlebuttModel);
+    return this._scuttlebuttModel.createReadStream();
+  },
+  createWriteStream: function () {
+    Ember.assert('No scuttlebutt model was initialised on this instance', this._scuttlebuttModel);
+    return this._scuttlebuttModel.createWriteStream();
   }
 });
 
--- tests/unit/scuttlebutt/scuttlebutt_object_test.js
+++ tests/unit/scuttlebutt/scuttlebutt_object_test.js
@@ -20,3 +20,23 @@ test('it can create a readable Stream for sync', function () {
   ok(stream.readable);
 });
 
+test('it can create a writeable Stream for sync', function () {
+  var sbo = SbObject.create();
+  ok(typeof sbo.createWriteStream === 'function');
+  var stream = sbo.createWriteStream();
+  ok(stream.writable);
+});
+
+asyncTest('it can receive changes from a scuttlebutt stream', function () {
+  expect(1);
+  var document = require('scuttlebutt/model')();
+  var get = Ember.get;
+  var sbo = SbObject.create({
+    valueObserver: function () {
+      ok(get(sbo, 'hello') === 'world', 'key "hello" should contain "world"');
+    }.observes('hello')
+  });
+  document.createReadStream().pipe(sbo.createWriteStream());
+  document.set('hello', 'world');
+});
+
