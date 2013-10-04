570bc5b76b80e08b85daff31e27f998abb0f54b4
Florent Jaby <fjaby@octo.com>
2013-10-04 18:08:10 +0200

    forgot route hello

--- server/shoeRouter.js
+++ server/shoeRouter.js
@@ -3,6 +3,7 @@ var Router = require('stream-router');
 var controllers = require('./shoeControllers');
 
 var router = Router();
+router.addRoute('/hello', controllers.hello);
 router.addRoute('/player1/me', controllers.player1.me);
 router.addRoute('/player1/opponent', controllers.player1.opponent);
 router.addRoute('/player2/me', controllers.player2.me);
