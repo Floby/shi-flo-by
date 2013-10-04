4cb5c4c189d78dc9afd8c420d25b031968d8df9d
Florent Jaby <fjaby@octo.com>
2013-10-04 10:51:23 +0200

    rename test

--- tests/unit/controllers/application_test.js
+++ tests/unit/controllers/application_test.js
@@ -4,6 +4,6 @@ import ApplicationController from 'appkit/controllers/application';
 module('Application controller');
 
 
-test('controller has a KEYCODE_J property', function () {
+test('ApplicationController class has a KEYCODE_J property', function () {
   ok(Ember.get(ApplicationController, 'KEYCODE_J') === 74, 'keycode for J is 74');
 })
