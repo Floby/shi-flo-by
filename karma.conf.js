b62a1cd98e3821474f1a4ce93b42237bc9d94ff5
Florent Jaby <fjaby@octo.com>
2013-10-12 15:31:38 +0200

    add bootstrap with open sans

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'vendor/bootstrap/js/bootstrap.js',
       'vendor/loader.js',
       'vendor/npm-modules.js',
       'vendor/jquery/jquery.js',
