8109da0b2e346791236b98ca0402c0f9db216b89
Florent Jaby <fjaby@octo.com>
2013-11-12 14:09:19 +0100

    cleanup router

--- app/router.js
+++ app/router.js
@@ -1,9 +1,6 @@
 var router = Ember.Router.map(function(){
   this.route('player1');
   this.route('player2');
-  // this.resource('posts', function() {
-  //   this.route('new');
-  // });
 });
 
 export default router;
