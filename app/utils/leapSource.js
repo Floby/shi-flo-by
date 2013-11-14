dea98425a8ee9dd2a317a1f5fb0a87aaa099df96
Florent Jaby <fjaby@octo.com>
2013-11-14 15:40:25 +0100

    count fingers

--- app/utils/leapSource.js
+++ app/utils/leapSource.js
@@ -3,6 +3,8 @@ var controller = new Leap.Controller();
 
 var LeapSource = Ember.Object.extend({
   controller: controller,
+  online: Ember.computed.not('offline'),
+
   init: function () {
     var self = this;
     this.get('controller').on('deviceConnected', function() {
@@ -11,6 +13,15 @@ var LeapSource = Ember.Object.extend({
     this.get('controller').on('deviceDisconnected', function() {
       self.set('offline', true);
     });
+    this.get('controller').on('animationFrame', function(frame) {
+      self.onFrame(frame);
+    });
+  },
+
+  onFrame: function (frame) {
+    Ember.run(this, function () {
+      this.set('fingerCount', frame.fingers.length);
+    })
   }
 });
 
--- tests/unit/utils/leapSource_test.js
+++ tests/unit/utils/leapSource_test.js
@@ -34,3 +34,37 @@ test('sets an offline flag to true when the leap device disconnects and back to 
   });
   equal(false, leapSource.get('offline'), 'device is connected');
 });
+
+test('registers its onFrame method on the controller\'s frame event', function () {
+  expect(1);
+  var callbacks = {};
+  var source = LeapSource.create({
+    controller: {
+      on: function (event, cb) {
+        callbacks[event] = cb;
+      }
+    },
+    onFrame: function () {
+      ok(true, "we have been called");
+    }
+  });
+  callbacks['animationFrame']();
+});
+
+test('updates its fingerCount attribute on each frame', function () {
+  expect(3);
+  var callbacks = {};
+  var source = LeapSource.create({
+    controller: {
+      on: function (event, cb) {
+        callbacks[event] = cb;
+      }
+    }
+  });
+  callbacks['animationFrame']({fingers: [1,2]});
+  equal(2, source.get('fingerCount'), "we should see 2 fingers");
+  callbacks['animationFrame']({fingers: [1,2,3,4]});
+  equal(4, source.get('fingerCount'), "we should see 4 fingers");
+  callbacks['animationFrame']({fingers: []});
+  equal(0, source.get('fingerCount'), "we should see 0 fingers");
+});
