898d33d5ef3ec6676694f485f3cee5e1719a8ce1
Florent Jaby <fjaby@octo.com>
2013-11-19 16:19:53 +0100

    remove player1 and player2 controllers

--- app/controllers/player1.js
+++
@@ -1,7 +0,0 @@
-import PlayerController from 'appkit/controllers/player';
-
-var Player1Controller = PlayerController.extend({
-  playerId: 'player1'
-});
-export default Player1Controller;
-
--- app/controllers/player2.js
+++
@@ -1,8 +0,0 @@
-import PlayerController from 'appkit/controllers/player';
-
-var Player2Controller = PlayerController.extend({
-  playerId: 'player2'
-});
-export default Player2Controller;
-
-
